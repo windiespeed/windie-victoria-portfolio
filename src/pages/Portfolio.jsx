e8aae9e5244d3683cf750cf1b03e2562076ca26d
windiespeed <windiespeed@gmail.com>
2025-08-03 14:37:19 -0500

    Token update

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -80,6 +80,17 @@ export default function Portfolio() {
       </div>
 
 
+      {activeTab === "UX/UI" && (
+        <div className="figma-embed my-5 text-center">
+          <iframe
+            style={{ border: 'none', width: '100%', maxWidth: '900px', height: '500px' }}
+            src="https://embed.figma.com/proto/YECKxuTSE7mV7yZblbYwMw/Windie-Speed---UI-UX-Photo-App?node-id=9818-3975&p=f&scaling=scale-down&content-scaling=fixed&page-id=6301%3A57&starting-point-node-id=9818%3A3975&embed-host=share" allowfullscreen
+            title="Figma UX/UI Embed"
+          ></iframe>
+        </div>
+      )}
+
+
       <div ref={loaderRef} className="text-center py-4 text-muted">Loading more...</div>
 
       {/* Thumbnail Bar 
